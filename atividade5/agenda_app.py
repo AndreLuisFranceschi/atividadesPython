76b3272755b48dff1b80d1953b5b669998e497c4
André Luís Franceschi <64626405+AndreLuisFranceschi@users.noreply.github.com>
2022-10-05 01:15:01 -0300

    Instanciando classe Agenda

---
+++ atividade5/agenda_app.py
@@ -0,0 +1,11 @@
+from agenda import Agenda
+
+a1 = Agenda('Gabriel', 'Barbosa', 999055732)
+a2 = Agenda('Diego', 'Souza', 999394035)
+a3 = Agenda('Renato', 'Kaiser', 999308504)
+a4 = Agenda('Juliana', 'Souza', 991900237)
+
+a1.mostra_registro()
+a2.mostra_registro()
+a3.mostra_registro()
+a4.mostra_registro()
