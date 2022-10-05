9ff57d002502b97bc0eecb001a92cd622deb8259
André Luís Franceschi <64626405+AndreLuisFranceschi@users.noreply.github.com>
2022-10-04 22:46:54 -0300

    Definição das classes Aluno, Professor e Curso

---
+++ atividade5/cadastro.py
@@ -0,0 +1,18 @@
+class Aluno:
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula
+
+
+class Professor:
+    def __init__(self, nome, registro):
+        self.nome = nome
+        self.registro = registro
+
+
+class Curso:
+    def __init__(self, nome, id):
+        self.nome = nome
+        self.id = id
+
+
